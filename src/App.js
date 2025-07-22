a9846f5ec476d233c206f71f07fcfff411d88207
Hussein Atoui <husseinatoui20@gmail.com>
2025-07-23 01:36:42 +0300

    Frontend React Setup
    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Donation Platform</h1>
+      {/* Your components will go here */}
+    </div>
+  );
+}
+
+export default App;
