61a03c35e629fdd26852ccfb3e7f2a7415323ccb
Hussein Atoui <husseinatoui20@gmail.com>
2025-10-18 22:56:52 +0300

    about us section fix

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import './AboutSection.css';
-import { ReactComponent as AboutIllustration } from '../../assets/about-illustration.svg';
+import AboutIllustration from '../../assets/login-illustration.jpg';
 
 const AboutSection = () => {
     return (
         <section className="about-section" id="about-us">
-            <div className="about-text">
-                <h2>About Us</h2>
-                <p className="about__description">
-                    At TyebeTyebak, we believe generosity shouldn’t be complicated —
-                    and help shouldn’t feel out of reach. We're a community-powered
-                    platform that connects your gently used clothing and essentials with local shelters,
-                    nonprofits, and families in need. Whether it’s a winter coat, a school bag, or a care kit
-                    — we help turn what you already have into real support for someone nearby.
-                    Every donation tells someone: you matter. Our mission is simple: make giving easier,
-                    more personal, and deeply human.
-                </p>
+            <div className="about-content">
+                <div className="about-text">
+                    <h2>About Us</h2>
+                    <p className="about__description">
+                        At TyebeTyebak, we believe generosity shouldn’t be complicated —
+                        and help shouldn’t feel out of reach. We're a community-powered
+                        platform that connects your gently used clothing and essentials with local shelters,
+                        nonprofits, and families in need. Whether it’s a winter coat, a school bag, or a care kit
+                        — we help turn what you already have into real support for someone nearby.
+                        Every donation tells someone: you matter. Our mission is simple: make giving easier,
+                        more personal, and deeply human.
+                    </p>
+                </div>
+
+                <div className="about-image">
+                    <img src={AboutIllustration} alt="About illustration" />
+                </div>
             </div>
 
-            <div className="about-image">
-                <AboutIllustration />
-            </div>
+            <div className="about-separator-bar"></div>
+
         </section>
+
     );
 };
 
